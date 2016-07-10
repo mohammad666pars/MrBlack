1cda11ad09fd5bc0c6f8e2c54671d169a49d83b2
mohammad666pars <mhs.6.mhs@gmail.com>
2016-07-10 00:44:30 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {mr_black10},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
